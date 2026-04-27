8856a64ccd8e25c7568a9cf601df4f08e9aa246b
Athulya <athulyaa7736@gmail.com>
2026-04-27 20:26:39 +0530

    Fix: add missing static, staticfiles, templates, chat folders

--- homeservice/settings.py
+++ homeservice/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-emmvrkilspmhyzzbuw!s!r6*irc2m@2$m4ozglxr1y1ckj6vz)'
+# SECRET_KEY = 'django-insecure-emmvrkilspmhyzzbuw!s!r6*irc2m@2$m4ozglxr1y1ckj6vz)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
